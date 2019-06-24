bec600419f0e4b79fe1bc7e0a17f90eb993641ac
liujun <1035841713@qq.com>
2019-06-25 00:05:53 +0800

    12

---
+++ SwiftTool/Example/NetworkRequestExampleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkRequestExampleViewController.swift
+//  SwiftTool
+//
+//  Created by 银河 on 2019/6/24.
+//  Copyright © 2019 yinhe. All rights reserved.
+//
+
+import UIKit
+
+class NetworkRequestExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+        
+    }
+
+}
